8680440df383dd235f8a692ff896d06de37133e0
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-14 23:46:00 +0700

    chore(cli-app): provide simple cli-app that uses getAll

--- bin/index.js
+++ bin/index.js
@@ -14,5 +14,37 @@ import {
 import chalk from "chalk";
 import boxen from "boxen";
 import figlet from "figlet";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 
-getAll();
+const usage =
+  boxen(
+    chalk.green(
+      "Lodde client app that enable us to call api requests from command"
+    ),
+    {
+      padding: 1,
+      borderColor: "green",
+      borderStyle: "double",
+    }
+  ) + "\ntype --help to get list of available commands \n";
+
+const argv = yargs(hideBin(process.argv))
+  .help()
+  .option("request", {
+    alias: "r",
+    type: "string",
+    description: "API Request Function",
+  })
+  .parse();
+
+if (argv.request == null) {
+  console.log(
+    chalk.yellow(figlet.textSync("Lodde-CLI", { horizontalLayout: "full" }))
+  );
+  console.log(usage);
+} else {
+  if (argv.request === "getAll") {
+    getAll();
+  }
+}
